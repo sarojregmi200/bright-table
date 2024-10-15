c4ec46c8813a95b1a4bf7a12b5cf78ab6a293ed1
sarojregmi200 <sarojregmi.official@gmail.com>
2024-10-15 11:20:40 +0545

    wip: Initialization and customization.
    - package link done.
    - rename prefix from `rs-table` to `bright-table` done.

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,35 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Cell, Column, HeaderCell, Table } from "bright-table"
+
+const data = [
+    {
+        "id": 1,
+        "firstname": "Chelsea",
+        "lastname": "Jast",
+        "gender": "male",
+        "age": 20,
+        "postcode": "74687-1119",
+        "email": "William75@hotmail.com"
+    },
+    {
+        "id": 2,
+        "firstname": "Michel",
+        "lastname": "Zieme",
+        "gender": "female",
+        "age": 45,
+        "postcode": "37099",
+        "email": "Santa87@yahoo.com"
+    },
+    {
+        "id": 3,
+        "firstname": "Chandler",
+        "lastname": "Leffler",
+        "gender": "female",
+        "age": 42,
+        "postcode": "31807",
+        "email": "Clemmie_Nolan38@yahoo.com"
+    },
+    {
+        "id": 4,
+        "firstname": "Freeman",
+        "lastname": "Schuppe",
+        "gender": "male",
+        "age": 46,
+        "postcode": "86958",
+        "email": "Stone59@yahoo.com"
+    },
+    {
+        "id": 7,
+        "firstname": "Mellie",
+        "lastname": "Kreiger",
+        "gender": "male",
+        "age": 29,
+        "postcode": "34476",
+        "email": "Stuart_Muller@gmail.com"
+    },
+    {
+        "id": 8,
+        "firstname": "Verna",
+        "lastname": "Stroman",
+        "gender": "female",
+        "age": 43,
+        "postcode": "22524-0074",
+        "email": "Cheyenne61@gmail.com"
+    },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Table
+            height={400}
+            data={data}
+            onRowClick={rowData => {
+                console.log(rowData);
+            }}
+        >
+            <Column width={60} align="center" fixed>
+                <HeaderCell>Id</HeaderCell>
+                <Cell dataKey="id" />
+            </Column>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <Column width={150}>
+                <HeaderCell>First Name</HeaderCell>
+                <Cell dataKey="firstName" />
+            </Column>
+
+            <Column width={150}>
+                <HeaderCell>Last Name</HeaderCell>
+                <Cell dataKey="lastName" />
+            </Column>
+
+            <Column width={100}>
+                <HeaderCell>Gender</HeaderCell>
+                <Cell dataKey="gender" />
+            </Column>
+
+            <Column width={100}>
+                <HeaderCell>Age</HeaderCell>
+                <Cell dataKey="age" />
+            </Column>
+
+            <Column width={150}>
+                <HeaderCell>Postcode</HeaderCell>
+                <Cell dataKey="postcode" />
+            </Column>
+
+            <Column width={300}>
+                <HeaderCell>Email</HeaderCell>
+                <Cell dataKey="email" />
+            </Column>
+            <Column width={80} fixed="right">
+                <HeaderCell>...</HeaderCell>
+
+                <Cell style={{ padding: '6px' }}>
+                    {rowData => (
+                        <button onClick={() => alert(`id:${rowData.id}`)}>
+                            Edit
+                        </button>
+                    )}
+                </Cell>
+            </Column>
+        </Table>
+    )
 }
 
 export default App
