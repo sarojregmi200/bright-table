b051ba64b67f65f05228992186b5518e5d0b3bd6
sarojregmi200 <sarojregmi.official@gmail.com>
2024-10-15 13:00:59 +0545

    chore: documentation and progress

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -59,59 +59,94 @@ const data = [
 
 function App() {
     return (
-        <Table
-            height={400}
-            data={data}
-            onRowClick={rowData => {
-                console.log(rowData);
-            }}
-        >
-            <Column width={60} align="center" fixed>
-                <HeaderCell>Id</HeaderCell>
-                <Cell dataKey="id" />
-            </Column>
+        <div>
+            <h2>
+                Default unstyled bright table
+            </h2>
+            <span>
+                This contains the following additional features:
+                <br />
+                <strong> Done: </strong>
+                <li>Auto import css file. </li>
+                <br />
+                <br />
 
-            <Column width={150}>
-                <HeaderCell>First Name</HeaderCell>
-                <Cell dataKey="firstName" />
-            </Column>
+                <strong> TODO: </strong>
+                <li>Default pagination and it's helper methods. </li>
+                <li>Fix Overflow during hover fix.</li>
+                <li>
+                    Column visibility status controller.
+                    <br />
+                    where each column has a hidden flag, always_visible flag, Editable flag.
+                </li>
+                <li>
+                    Implementing bright design, and better documentation.
+                </li>
+                <li>
+                    Selectable row, with global selection range selection while holding shift and similar features from current table.
+                </li>
+                <li>
+                    Collapsable rows, when given nested data.
+                </li>
+                <li>
+                    Tailwind support.
+                </li>
+            </span>
 
-            <Column width={150}>
-                <HeaderCell>Last Name</HeaderCell>
-                <Cell dataKey="lastName" />
-            </Column>
+            <Table
+                height={400}
+                data={data}
+                onRowClick={rowData => {
+                    console.log(rowData);
+                }}
+            >
+                <Column width={60} align="center" fixed>
+                    <HeaderCell>Id</HeaderCell>
+                    <Cell dataKey="id" />
+                </Column>
 
-            <Column width={100}>
-                <HeaderCell>Gender</HeaderCell>
-                <Cell dataKey="gender" />
-            </Column>
+                <Column width={150}>
+                    <HeaderCell>First Name</HeaderCell>
+                    <Cell dataKey="firstName" />
+                </Column>
 
-            <Column width={100}>
-                <HeaderCell>Age</HeaderCell>
-                <Cell dataKey="age" />
-            </Column>
+                <Column width={150}>
+                    <HeaderCell>Last Name</HeaderCell>
+                    <Cell dataKey="lastName" />
+                </Column>
 
-            <Column width={150}>
-                <HeaderCell>Postcode</HeaderCell>
-                <Cell dataKey="postcode" />
-            </Column>
+                <Column width={100}>
+                    <HeaderCell>Gender</HeaderCell>
+                    <Cell dataKey="gender" />
+                </Column>
 
-            <Column width={300}>
-                <HeaderCell>Email</HeaderCell>
-                <Cell dataKey="email" />
-            </Column>
-            <Column width={80} fixed="right">
-                <HeaderCell>...</HeaderCell>
+                <Column width={100}>
+                    <HeaderCell>Age</HeaderCell>
+                    <Cell dataKey="age" />
+                </Column>
 
-                <Cell style={{ padding: '6px' }}>
-                    {rowData => (
-                        <button onClick={() => alert(`id:${rowData.id}`)}>
-                            Edit
-                        </button>
-                    )}
-                </Cell>
-            </Column>
-        </Table>
+                <Column width={150}>
+                    <HeaderCell>Postcode</HeaderCell>
+                    <Cell dataKey="postcode" />
+                </Column>
+
+                <Column width={300}>
+                    <HeaderCell>Email</HeaderCell>
+                    <Cell dataKey="email" />
+                </Column>
+                <Column width={80} fixed="right">
+                    <HeaderCell>...</HeaderCell>
+
+                    <Cell style={{ padding: '6px' }}>
+                        {rowData => (
+                            <button onClick={() => alert(`id:${rowData.id}`)}>
+                                Edit
+                            </button>
+                        )}
+                    </Cell>
+                </Column>
+            </Table>
+        </div >
     )
 }
 
