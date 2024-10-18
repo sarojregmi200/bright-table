f359d4e1e6e790582269ce2246ed0960b7e9618c
sarojregmi200 <sarojregmi.official@gmail.com>
2024-10-18 11:41:46 +0545

    patch(gulp): reset gulp for current development

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,23 @@
-import { del } from 'del';
-import { babel } from 'gulp-babel';
-import { less } from 'gulp-less';
-import { postcss } from 'gulp-postcss';
-import sourcemaps from 'gulp-sourcemaps';
-import rename from 'gulp-rename';
-import insert from 'gulp-insert';
-import gulp from 'gulp';
-import babelrc from './babel.config.js';
-import browserSync from 'browser-sync';
-
-const LESS_SOURCE_DIR = './src/less';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const del = require('del');
+const babel = require('gulp-babel');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const insert = require('gulp-insert');
+const gulp = require('gulp');
+const babelrc = require('./babel.config.js');
+const STYLE_SOURCE_DIR = './src/less';
 const STYLE_DIST_DIR = './dist/css';
 const TS_SOURCE_DIR = ['./src/**/*.tsx', './src/**/*.ts', '!./src/**/*.d.ts'];
 const ESM_DIR = './es';
 const CJS_DIR = './lib';
 const DIST_DIR = './dist';
 
-const paths = {
-  LESS_SOURCE_DIR: './src/less',
-  STYLE_DIST_DIR: './dist/css',
-  DIST_DIR: './dist',
-  TS_SOURCE_DIR: ['./src/**/*.tsx', './src/**/*.ts', '!./src/**/*.d.ts'],
-  DOCS_DIR: './docs'
-};
-
-function reload() {
-  browserSync.reload();
-  done();
-}
-
-function server() {
-  browserSync.init({
-    proxy: 'localhost:3000',
-    server: {
-      baseDir: paths.DOCS_DIR
-    },
-    open: false,
-    notify: true
-  });
-
-  done();
-}
-
 function buildLess() {
   return gulp
-    .src(`${LESS_SOURCE_DIR}/index.less`)
+    .src(`${STYLE_SOURCE_DIR}/index.less`)
     .pipe(sourcemaps.init())
     .pipe(less({ javascriptEnabled: true }))
     .pipe(postcss([require('autoprefixer')]))
@@ -53,7 +26,7 @@ function buildLess() {
     .pipe(gulp.dest(`${STYLE_DIST_DIR}`));
 }
 
-function minifyCSS() {
+function buildCSS() {
   return gulp
     .src(`${STYLE_DIST_DIR}/bright-table.css`)
     .pipe(sourcemaps.init())
@@ -61,6 +34,17 @@ function minifyCSS() {
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(`${STYLE_DIST_DIR}`));
+}
+
+function buildLib() {
+  return (
+    gulp
+      .src(TS_SOURCE_DIR)
+      .pipe(babel(babelrc()))
+      // adds the 'use-client' directive to /lib exported from bright-talbe
+      .pipe(insert.prepend(`'use client';\n`))
+      .pipe(gulp.dest(CJS_DIR))
+  );
 }
 
 function buildEsm() {
@@ -88,35 +72,31 @@ function copyLessFiles() {
     .pipe(gulp.dest(ESM_DIR));
 }
 
+function copyFontFiles() {
+  return gulp.src(`${STYLE_SOURCE_DIR}/fonts/**/*`).pipe(gulp.dest(`${STYLE_DIST_DIR}/fonts`));
+}
+
 function clean(done) {
   del.sync([CJS_DIR, ESM_DIR, DIST_DIR], { force: true });
   done();
 }
 
-// creates a development build and watch for change.
 function watchFiles() {
   gulp.watch(
     'src/**/*',
     gulp.series(
       clean,
-      gulp.parallel(buildLib, buildEsm, gulp.series(buildLess, minifyCSS)),
+      gulp.parallel(buildLib, buildEsm, gulp.series(buildLess, buildCSS)),
       gulp.parallel(copyTypescriptDeclarationFiles, copyLessFiles, copyFontFiles)
     )
   );
 }
 
-// creates a production build ready to publish.
-const build = gulp.series(
+exports.build = gulp.series(
   clean,
-  gulp.parallel(buildLib, buildEsm, gulp.series(buildLess, minifyCSS)),
+  gulp.parallel(buildLib, buildEsm, gulp.series(buildLess, buildCSS)),
   gulp.parallel(copyTypescriptDeclarationFiles, copyLessFiles, copyFontFiles)
 );
 
-// cleanup
-process.on('exit', stopDevServer);
-process.on('SIGINT', process.exit);
-
-exports.build = build;
-
 // watching for file changes
-exports.default = watchFiles;
+exports.default = gulp.series(watchFiles);
